95f10841ddfd1b66c64039eb3a492eda476ba0a9
David Eidelman <eidelman.david@gmail.com>
2024-03-24 15:54:18 -0400

    finalized baglab environment

--- rdsad/__init__.py
+++
@@ -1,3 +0,0 @@
-from .seurat import read_10x_data
-from .seurat import read_rds_file
-from .seurat import create_adata
--- rdsad/__main__.py
+++
@@ -1,7 +0,0 @@
-#
-# rdsad.__main__.py
-#
-# entry point for system that aims to translate .rds files into .h5ad files
-#
-
-print("Not yet implemented")
